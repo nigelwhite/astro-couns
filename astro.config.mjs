8b496227adc032a2b3bbe502018eccb81f872f5d
nigelwhite <webdev@permaculture.org.uk>
2024-01-30 18:04:32 +0000

    couple images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+// import { astroImageTools } from "astro-imagetools";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
 });
