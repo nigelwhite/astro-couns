b0e329a7b6e1778155747ca9b3048bd7444ff432
nigelwhite <webdev@permaculture.org.uk>
2024-01-31 06:03:38 +0000

    install astroImageTools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
-// import { astroImageTools } from "astro-imagetools";
+import { astroImageTools } from "astro-imagetools";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), astroImageTools],
 });
